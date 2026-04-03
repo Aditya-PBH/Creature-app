38bdc912f723938845b3ad0ed35a8ab309775d59
Aditya-PBH <pandeyaditya3999@gmail.com>
2026-04-03 16:25:02 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,26 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyDo_49ukgSc32Dc1wqbRbQA3yGLk8bblR0",
+  authDomain: "creature-62507.firebaseapp.com",
+  projectId: "creature-62507",
+  storageBucket: "creature-62507.firebasestorage.app",
+  messagingSenderId: "746633251563",
+  appId: "1:746633251563:web:53cde0d618efc09e538cae",
+  measurementId: "G-VKZCYWH4VF"
 };
 
 const app = initializeApp(firebaseConfig);
+
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
+export const db = getFirestore(app);
+export const storage = getStorage(app);
 
 export default app;
